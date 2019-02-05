5754e79db9c78f779e5a5f9f59f9908b461c9b2c
Lawgiver17 <ccolon105@yahoo.com>
2019-02-05 13:32:22 -0700

    added auth files :zap:

---
+++ models/Users.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+  username: {
+      type: String,
+      unique: true,
+      required: [true, "username is required"]
+  },
+  password: {
+      type: String,
+      unique: false,
+    //   validate: {
+    //     validator: function(v) {
+    //       return /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/.test(v);
+    //     },
+    //     message: props => `${props.value} is not a valid password`
+    //   },
+    //   required: [true, "password is required"]
+  },
+  admin: {
+    type: Boolean,
+    unique: false,
+    required: true,
+    default: false
+},
+  createdAt: {
+      type: Date,
+      default: Date.now()
+  }
+});
+
+usersSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
+
+usersSchema.methods.validPassword = function(password, encrypted) {
+    return bcrypt.compareSync(password, encrypted);
+}
+
+const User = mongoose.model("User", usersSchema);
+
+module.exports = User;
