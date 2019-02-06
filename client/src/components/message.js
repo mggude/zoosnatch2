46c720f4bcd9e6a71b992fbf0daa306780c8f36c
Grace Gude <grace@Graces-MacBook-Pro.local>
2019-02-06 13:54:52 -0700

    message

--- client/src/components/message.js
+++ client/src/components/message.js
@@ -1,5 +1,5 @@
 import React from "react";
-import characterObject from "../../characters.json";
+import characterObject from "../characters.json";
 import { Container, Row, Col, Button } from "reactstrap";
 
 export const Message = (props) => {
